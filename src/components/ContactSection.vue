23a3a440dbdce5bb8199b7abc8937a4b66408c81
skultan <y.skultan@gmail.com>
2021-01-16 02:35:18 +0700

    init

---
+++ src/components/ContactSection.vue
@@ -0,0 +1,100 @@
+<template>
+  <section class="pb-8" id="contact">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row justify="center">
+            <v-col cols="12" sm="5">
+              <h1 class="font-weight-light display-1">Contact us</h1>
+              <h3 class="font-weight-light mt-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                explicabo commodi quisquam asperiores dolore ad enim provident
+                veniam perferendis voluptate, perspiciatis.
+              </h3>
+              <h3 class="font-weight-light mt-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+              </h3>
+              <h3 class="font-weight-light mt-3">
+                Phone number: +x (xxx) xxx-xxxx
+              </h3>
+              <h3 class="font-weight-light">
+                Email: email@email.com
+              </h3>
+            </v-col>
+            <v-col cols="12" sm="7">
+              <v-form @submit="onSubmit" ref="form" v-model="valid" :lazy-validation="lazy">
+                <v-text-field
+                  v-model="name"
+                  label="Name"
+                  required
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="email"
+                  label="E-mail"
+                  required
+                ></v-text-field>
+
+                <v-textarea
+                  v-model="textArea"
+                  label="Message"
+                  required
+                />
+
+                <v-btn
+                  :disabled="!valid"
+                  color="primary"
+                  :dark="valid"
+                  rounded
+                  block
+                  class="mt-3"
+                  type="submit"
+                >
+                  Send
+                </v-btn>
+              </v-form>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="svg-border-waves text-white">
+      <v-img src="~@/assets/img/borderWavesBlue.svg" />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+#contact {
+  background-color: #f4f7f5;
+}
+
+.svg-border-waves .v-image {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 3rem;
+  width: 100%;
+  overflow: hidden;
+}
+
+</style>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["fa-facebook", "fa-twitter", "fa-linkedin", "fa-instagram"],
+    valid: true,
+    name: "",
+    email: "",
+    textArea: "",
+    lazy: false,
+  }),
+  methods: {
+    onSubmit (e) {
+      console.log("teste")
+      e.preventDefault();
+    }
+  }
+};
+</script>
